a83b175f60414c093620246d15a6b20669decb6b
fengyushang <1135920209@qq.com>
2017-08-24 14:14:10 +0800

    dropdown

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { composeTheme,getColorByType } from '../helpers';
+import Button from 'react-sui/Button';
 class Dropdown extends Component {
+    static props = {
+        type: PropTypes.string,
+        size: PropTypes.string,
+        label: PropTypes.string,
+        options: PropTypes.array,
+    };
+    static defaultProps = {
+        type: 'default'
+    };
     render(){
+        const { type,t,size,label } = this.props;
+        return (
+            <div>
+                <Button label={ label } type={ type }/>
 
+            </div>
+        )
     }
 }
 
