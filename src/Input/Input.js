8e7c75d2201149993d74feb11fadd5c673b483be
sangzhenlin <17010282446@163.com>
2017-09-26 10:09:06 +0800

    Input Component

---
+++ src/Input/Input.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import {composeTheme} from "../helpers";
+import PropTypes from 'prop-types';
+import {autobind} from 'core-decorators';
+
+@autobind
+
+class Input extends Component {
+    static props = {
+        name: PropTypes.string,
+        value: PropTypes.string,
+        change: PropTypes.func,
+        placeholder: PropTypes.string,
+        maxlength: PropTypes.number,
+        unit: PropTypes.string,
+        type: PropTypes.string,
+        formatter: PropTypes.func,
+        parse: PropTypes.func,
+        min: PropTypes.number,
+    };
+
+    onChange(e) {
+        const {value} = e.target;
+        const {name, change, parser, maxlength} = this.props;
+         if (maxlength && value.length > maxlength) {
+             let valStr = value.slice(0, maxlength);
+             this.refs.input.value = valStr;
+             change(name, value);
+         } else {
+            change(name, value);
+         }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {value} = this.props;
+        if (nextProps.value != this.props.value) {
+            if (value) {
+                this.refs.input.value = value;
+            } else {
+                this.refs.input.value = '';
+            }
+        }
+    }
+
+    render() {
+        const {type, value, maxlength, placeholder, min, unit,formatter} = this.props;
+        let newValue = value;
+        if (formatter) {
+            newValue = formatter(value);
+        }
+        return (
+            <div style={{
+                position: 'relative'
+            }}>
+                <input type={type || "text"} value={newValue} ref="input" className="form-control"
+                       onBlur={this.onChange} maxLength={maxlength || 999} onChange={ this.onChange }
+                       placeholder={placeholder} step="any" min={min}/>
+                <span style={{
+                    position: 'absolute',
+                    right: '.25rem',
+                    top: '.4rem'
+                }}>{unit || ''}</span>
+            </div>
+        )
+    }
+}
+
+export default composeTheme(Input);
